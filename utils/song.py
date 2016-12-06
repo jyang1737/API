a429edb1368b1ec16ebf182400738d4c932291c4
nicolecheng <ncheng@stuy.edu>
2016-12-06 14:47:35 -0500

    update song.py

---
+++ utils/song.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#<img src="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuXW">
+#script type="text/javascript" src="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuOP"
+
+
+# search for song
+#track.search?q_track=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1
+#track.search?q_lyrics=music%20hack%20day
+
+# get lyrics after search is done
+#track.lyrics.get?track_id=15953433
+
+# convert json to plain text
+#https://docs.python.org/2/library/json.html
