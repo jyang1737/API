1c45f6672cd01c6a3fac153778e12819f91be81c
nicolecheng <ncheng@stuy.edu>
2016-12-07 02:30:36 -0500

    invalid authorization

--- utils/musixmatch.py
+++ utils/musixmatch.py
@@ -6,12 +6,30 @@
 # @see https://developer.musixmatch.com/documentation
 # @copy 2016 Musixmatch Spa
 
+
+import six
+import urllib2
+import urllib3
+
+
+
 import time
 import sys
 
 import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
+
+url="http://api.musixmatch.com/ws/1.1/tracking.url.get?apikey=5f423b7772a80f77438407c8b78ff305&format=json"
+endurl="http://api.musixmatch.com/ws/1.1/"
+
+
+
+req = urllib2.Request(url+'track.search?q_lyrics=music%20hack%20day')#,data=None)
+response = urllib2.urlopen(req)
+the_page = response.read()
+print the_page
+
 
 if len(sys.argv) <= 1:
     print "\nUsage: python musixmatch.py 5e77a0cfadb9f7b386968d9150c3d0f2";
@@ -31,3 +49,4 @@ try:
     pprint(api_response)
 except ApiException as e:
     print "Exception when calling DefaultApi->album_get_get: %s\n" % e
+
--- utils/song.py
+++ utils/song.py
@@ -1,15 +1,61 @@
 #!/usr/bin/env python
 
-#<img src="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuXW">
-#script type="text/javascript" src="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuOP"
+import json
+
+import urllib2
+
+
+
+#class urllib2.Request(url[, data][, headers][, origin_req_host][, unverifiable])
 
 
 # search for song
 #track.search?q_track=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1
 #track.search?q_lyrics=music%20hack%20day
 
+def get_song(L):
+    s = "track.search?q_lyrics="
+    n = 0
+    while n < len(L):
+        s += L[n]
+        if n < (len(L) - 1):
+            s += "%20"
+        n += 1
+    return s
+
+#print get_song(["music","hack","day"])
+'''
+L = ["music","hack","day"]
+req = urllib2.Request('http://api.musixmatch.com/ws/1.1/'+get_song(L))
+response = urllib2.urlopen(req)
+the_page = response.read()
+
+print the_page
+'''
+
+
 # get lyrics after search is done
 #track.lyrics.get?track_id=15953433
 
-# convert json to plain text
+# parse json
 #https://docs.python.org/2/library/json.html
+def get_lyrics():
+	url = genURL(x, y)
+	curr = urllib2.urlopen(url)
+	req = curr.read()
+	reqdict = json.loads(req)
+	retdict = {}
+	retdict["icon"] = getIcon(x, y, offset, reqdict)
+
+
+#<img src="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuXW">
+#script type="text/javascript" src="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuOP"
+
+# download https://github.com/musixmatch/musixmatch-sdk/blob/master/dist/python-client-generated.zip
+# pip install six
+# pip install urllib3
+# pip install certifi
+
+
+#https://developer.musixmatch.com/documentation/rights-clearance-on-your-existing-catalog
+#https://developer.musixmatch.com/documentation/api-reference/tracking-url-get
