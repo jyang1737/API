daf0e7ccb907184928b070169f6070c903cbbeaa
IAsWell <jyang1737@gmail.com>
2016-12-07 14:38:39 -0500

    fixed linking

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
+app.secret_key = 'ajfkdsjflkasd'
 
 @app.route("/")
 @app.route("/home/")
 def main():
+    return render_template("main.html")
 
-
-@app.route("/s/<query>", methods=["GET","POST"])
-def query(query):
-
+@app.route("/s/", methods=["POST"])
+def query():
+    response = request.form
+    image = response["img"]
+    return render_template("navbar.html")
 
 @app.route("/s/<song>")
 def song(song):
+    return render_template("main.html")
 
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
 
 
 
