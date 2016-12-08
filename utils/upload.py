6fb08583efee64c82622d68c4812f16058763ce1
maxbert <mbertfield@stuy.edu>
2016-12-08 14:47:52 -0500

    wrote getlistcolros(path)

--- utils/upload.py
+++ utils/upload.py
@@ -2,7 +2,7 @@ import json, urllib2, urllib, os, mimetypes, tempfile, httplib
 
 def getlist(path):
     key = getkey()
-    url = "https://api.clarifai.com/v1/tag/?url="+path+"&access_token="+key
+    url = "https://api.clarifai.com/v1/tag/"
     data = {"url":path}
     values = urllib.urlencode(data)
     headers = {"Authorization" : "Bearer " + key}
@@ -11,6 +11,18 @@ def getlist(path):
     r = u.read()
     d = json.loads(r)
     return d['results'][0]['result']['tag']['classes']
+
+def getlistcolors(path):
+    key = getkey()
+    url = "https://api.clarifai.com/v1/color/"
+    data = {"url":path}
+    values = urllib.urlencode(data)
+    headers = {"Authorization" : "Bearer " + key}
+    request = urllib2.Request(url,values,headers = headers)
+    u = urllib2.urlopen(request)
+    r = u.read()
+    d = json.loads(r)
+    return {d['results'][0]['colors'][0]['w3c']['hex'], d['results'][0]['colors'][2]['w3c']['hex']}
 
 def getlistlocal(path):
     key = getkey()
@@ -30,9 +42,7 @@ def getkey():
     f = open('../secretdata.txt', 'r')
     lines = f.readlines()
     iid = lines[0].strip('\n')
-   
     secret = lines[1].strip('\n')
-   
     url = "https://api.clarifai.com/v1/token"
     data = urllib.urlencode({"client_id":iid, 'client_secret':secret, "grant_type":"client_credentials"})
     request = urllib2.Request(url,data)
